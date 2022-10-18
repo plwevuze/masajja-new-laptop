53b81d1c15e93d95157e5274b3d1370b17b93204
ghostPaul <s.p.lwevuze@gmail.com>
2022-10-18 13:30:08 -0700

    posting to database

---
+++ routes/registerAORoutes.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+//instatiate a file that points to the express to access different methods
+const router = express.Router();
+
+//creating/writing a route
+router.get('/', (req, res) => {
+	res.render('agriculturalOffcer');
+});
+
+
+//post route
+router.post('/', (req, res) => {
+	console.log(req.body);
+	res.send('Agricultural Officer Registration Successfull!!!!!');
+	// res.redirect('/register/farmerOne');
+});
+
+//always the last line in the route files
+//exposes the router file to be used in another file
+module.exports = router;
